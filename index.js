a6a3c73d5210a666e41257c9e977f6e163c6342e
Shobhit Raj <92620327+shobhit-28@users.noreply.github.com>
2022-10-04 08:46:21 +0000

    added some questions

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
 var score = 0;
 var questions = [
   q1 = {
-    question:'What is my name?',
-    answer:'Shobhit',
+    question: 'What is my name?',
+    answer: 'Shobhit',
   },
   q2 = {
-    question:'Where was I born?',
-    answer:'Bihar',
+    question: 'Where was I born?',
+    answer: 'Bihar',
   },
   q3 = {
-    question:'Which game do I like the most?',
-    answer:'Valo',
+    question: 'Which game do I like the most?',
+    answer: 'Valo',
+  },
+  q4 = {
+    question: 'Where did I did all my schooling?',
+    answer: 'Jharkhand',
+  },
+  q5 = {
+    question: 'Which degree I am pursuing currently?',
+    answer: 'Btech',
+  },
+  q6 = {
+    question: 'Which sports I used to play in 12th?',
+    answer: 'Basketball',
   },
 ];
 
 
-function checkAns(question, answer){
-  if(que === answer){
+function checkAns(question, answer) {
+  if (que === answer) {
     console.log('You are correct');
-    score ++;
+    score++;
   }
-  else{
+  else {
     console.log('You are wrong');
-    score --;
+    score--;
   }
 }
 
+console.log('This is a small quiz on me to see how much you know me and enter your answers with 1st letter capital\n');
 
-for(var init = 0; init < questions.length; init++){
+for (var init = 0; init < questions.length; init++) {
   var input = require('readline-sync');
   var que = input.question(questions[init].question + '\n');
   checkAns(que, questions[init].answer);
+  if (init === questions.length - 1) {
+    console.log('\n')
+  }
   console.log('Total score is ' + score);
+  console.log('\n');
 }
